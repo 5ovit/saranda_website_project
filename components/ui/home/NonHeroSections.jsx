f3c7481c64e233d20b87b8fa8080e1fb876275aa
5ovit <5ovit@users.noreply.github.com>
2025-11-03 12:47:44 +0530

    refactor: change component names
    CTAButton -> Button
    LandinPageSections -> NonHeroSections

--- components/ui/home/NonHeroSections.jsx
+++ components/ui/home/NonHeroSections.jsx
@@ -3,31 +3,26 @@ import mist_forest_img from "@/public/images/mist_forest2.png";
 import lone_tree_img from "@/public/images/lone_tree.png";
 import { twJoin } from "tailwind-merge";
 
-export default function LandingPageSections({
+export default function NonHeroSections({
   className = "",
   children,
   ...props
 }) {
-  const landing_page_sections_id = "landing_page_sections_id"
-  const landing_page_bg_container_id = "landing_page_bg_container_id"
-  const lone_tree_img_id = "lone_tree_img_id"
-  const mist_forest_img_id = "mist_forest_img_id"
-
   return <>
     <main
       className={twJoin(
         "relative",
         className
       )}
-      id={landing_page_sections_id}
+      id="landing_page_sections_id"
       {...props}
     >
       <div
         className="absolute inset-0"
-        id={landing_page_bg_container_id}
+        id="landing_page_bg_container_id"
       >
         <Image
-          id={mist_forest_img_id}
+          id="mist_forest_img_id"
           src={mist_forest_img}
           alt=""
           sizes="100vw"
@@ -40,12 +35,12 @@ export default function LandingPageSections({
       >
         <Image
           src={lone_tree_img}
-          alt="Lone Tree"
+          alt=""
           sizes="100vw"
           className="
             object-fill w-screen h-screen sticky top-0
           "
-          id={lone_tree_img_id}
+          id="lone_tree_img_id"
         ></Image>
       </div>
       {children}
